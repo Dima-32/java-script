664be14c3829112863781622e9b7509920bd5a94
Dima-32 <pylypiukdmytro32@gmail.com>
2025-11-30 14:07:02 +0100

    Add files via upload

---
+++ pylypiuk_zewnetrzny.js
@@ -0,0 +1,6 @@
+function okno_zamknij_pylypiuk()  
+                       { 
+                             window.close() 
+                       } 
+document.write("ostatnia modyfikacja strony".fontcolor("olive").strike().bold().fontsize(5)+"<br>"); 
+document.write(document.lastModified.fontcolor("lime").bold().fontsize(5)+"<br>");
